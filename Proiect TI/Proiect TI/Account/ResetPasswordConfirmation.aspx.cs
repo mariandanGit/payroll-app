dcc18175f4c6042d234c4175909d0012c4eaa2c5
Marian Dan <danmarian713@gmail.com>
2023-01-27 12:00:47 +0200

    Set up basic project

---
+++ Proiect TI/Proiect TI/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Proiect_TI.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Proiect TI/Proiect TI/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proiect_TI.Startup))]
+namespace Proiect_TI
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
